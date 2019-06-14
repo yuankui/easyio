7e3be6c956c076028c7a8912c1bf000f81d81868
yuankui <yuankui64@gmail.com>
2019-06-14 13:37:14 +0800

    add async test

--- easyio-runner-es/src/test/java/io/github/yuankui/easyio/runner/es/EsRunnerTest.java
+++ easyio-runner-es/src/test/java/io/github/yuankui/easyio/runner/es/EsRunnerTest.java
@@ -1,6 +1,7 @@
 package io.github.yuankui.easyio.runner.es;
 
 import org.apache.http.HttpHost;
+import org.elasticsearch.action.ActionListener;
 import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.RequestOptions;
@@ -10,8 +11,12 @@ import org.elasticsearch.common.unit.TimeValue;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.junit.Test;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Scheduler;
+import reactor.core.scheduler.Schedulers;
 
 import java.io.IOException;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
 
 public class EsRunnerTest {
@@ -35,5 +40,46 @@ public class EsRunnerTest {
         SearchResponse response = client.search(request, options);
 
         System.out.println("response = " + response);
+    }
+
+    @Test
+    public void async() throws InterruptedException {
+        RestHighLevelClient client = new RestHighLevelClient(
+                RestClient.builder(
+                        new HttpHost("localhost", 9200, "http"),
+                        new HttpHost("localhost", 9201, "http")));
+
+        SearchRequest request = new SearchRequest();
+        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
+        sourceBuilder.query(QueryBuilders.termQuery("name", "yuankui"));
+        sourceBuilder.from(0);
+        sourceBuilder.size(5);
+        sourceBuilder.timeout(new TimeValue(60, TimeUnit.SECONDS));
+
+        request.source(sourceBuilder);
+
+        RequestOptions options = RequestOptions.DEFAULT;
+
+        Mono<SearchResponse> mono = Mono.create(sink -> {
+            client.searchAsync(request, options, new ActionListener<SearchResponse>() {
+                @Override
+                public void onResponse(SearchResponse searchResponse) {
+                    sink.success(searchResponse);
+                }
+
+                @Override
+                public void onFailure(Exception e) {
+                    sink.error(e);
+                }
+            });
+        });
+
+        Scheduler scheduler = Schedulers.single();
+        mono.publishOn(scheduler)
+                .subscribe(resp -> {
+                    System.out.println("resp = " + resp);
+                });
+        
+        TimeUnit.SECONDS.sleep(10);
     }
 }
