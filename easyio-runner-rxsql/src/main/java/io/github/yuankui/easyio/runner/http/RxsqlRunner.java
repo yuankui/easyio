40f508bad23f34a6695048175baa181207178c04
yuankui <yuankui64@gmail.com>
2020-01-15 20:08:57 +0800

    add rxsql provider

---
+++ easyio-runner-rxsql/src/main/java/io/github/yuankui/easyio/runner/http/RxsqlRunner.java
@@ -0,0 +1,30 @@
+package io.github.yuankui.easyio.runner.http;
+
+import io.github.yuankui.easyio.generic2.GenericRunner;
+import io.github.yuankui.easyio.generic2.Provider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class RxsqlRunner extends GenericRunner {
+    
+    @Autowired
+    private ApplicationContext context;
+    
+    @Override
+    public List<Provider> getProviderList() {
+
+        List<Provider> providers = context.getBeansWithAnnotation(RxsqlProvider.class)
+                .values()
+                .stream()
+                .filter(i -> i instanceof Provider)
+                .map(i -> (Provider) i)
+                .collect(Collectors.toList());
+
+        return providers;
+    }
+}
