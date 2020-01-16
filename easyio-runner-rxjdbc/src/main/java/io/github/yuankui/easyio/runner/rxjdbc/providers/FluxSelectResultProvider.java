5676d2fe37bd21bffbd207d27d648e7eb6b8ec33
yuankui <yuankui64@gmail.com>
2020-01-16 15:52:37 +0800

    up

--- easyio-runner-rxjdbc/src/main/java/io/github/yuankui/easyio/runner/rxjdbc/providers/FluxSelectResultProvider.java
+++ easyio-runner-rxjdbc/src/main/java/io/github/yuankui/easyio/runner/rxjdbc/providers/FluxSelectResultProvider.java
@@ -1,5 +1,6 @@
 package io.github.yuankui.easyio.runner.rxjdbc.providers;
 
+import com.alibaba.fastjson.JSONObject;
 import io.github.yuankui.easyio.generic2.*;
 import io.github.yuankui.easyio.runner.rxjdbc.RxjdbcProvider;
 import io.reactivex.Flowable;
@@ -7,34 +8,45 @@ import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
 
 import java.lang.reflect.Method;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 import java.util.Collection;
 
 @RxjdbcProvider
 @Component
-public class FluxSelectResultProvider implements Provider<Object> {
+public class FluxSelectResultProvider implements Provider<Flux<Object>> {
     @Override
-    public Result<Object> init(Method method, InitContext context) {
-        if (method.getReturnType() != Flux.class) {
-            return Result.fail("return type not Flux");
-        }
-        
-        Collection<Resource<Flowable>> r = context.getResources("result");
-        if (r.isEmpty()) {
-            return Result.fail("need another result");
+    public Result<Flux<Object>> init(Method method, InitContext context) {
+        // return type
+        Type type;
+        Type genericReturnType = method.getGenericReturnType();
+        if (genericReturnType instanceof ParameterizedType) {
+            ParameterizedType parameterizedType = (ParameterizedType) genericReturnType;
+            if (parameterizedType.getRawType() != Flowable.class) {
+                return Result.fail("return type not Flowable");
+            }
+            type = parameterizedType.getActualTypeArguments()[0];
+        } else {
+            return Result.fail("return type not Flowable<T>");
         }
 
-        Callable<Flowable> callable = r.stream()
+        Collection<Resource<Flowable<JSONObject>>> selectResult = context.getResources("selectResult");
+        if (selectResult.isEmpty()) {
+            return Result.fail("missing @R-selectResult");
+        }
+
+        Callable<Flowable<JSONObject>> callable = selectResult.stream()
                 .reduce((o1, o2) -> o2)
                 .get()
                 .getCallable();
 
-        return Result.success(ioContext -> Flux.from(callable.call(ioContext)));
+        return Result.success(ioContext -> {
+            Flowable<Object> flowable = callable.call(ioContext)
+                    .map(json -> json.toJavaObject(type));
+            return Flux.from(flowable);
+        });
     }
 
-    @Override
-    public CompareResult compare(Provider<Object> competitor) {
-        return CompareResult.better(1, "I use his");
-    }
 
     @Override
     public String resourceName() {
---
+++ easyio-runner-rxjdbc/src/main/java/io/github/yuankui/easyio/runner/rxjdbc/providers/SelectResultProvider.java
@@ -0,0 +1,136 @@
+package io.github.yuankui.easyio.runner.rxjdbc.providers;
+
+import com.alibaba.fastjson.JSONObject;
+import io.github.yuankui.easyio.generic2.*;
+import io.github.yuankui.easyio.runner.rxjdbc.RxjdbcProvider;
+import io.reactivex.Flowable;
+import org.apache.commons.lang3.tuple.Pair;
+import org.davidmoten.rx.jdbc.Database;
+import org.davidmoten.rx.jdbc.SelectBuilder;
+import org.davidmoten.rx.jdbc.exceptions.SQLRuntimeException;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.util.Calendar;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+@RxjdbcProvider
+@Component
+public class SelectResultProvider implements Provider<Flowable<JSONObject>> {
+    @Override
+    public Result<Flowable<JSONObject>> init(Method method, InitContext context) {
+
+        // database
+        Collection<Resource<Database>> databases = context.getResources("database");
+        if (databases.isEmpty()) {
+            return Result.fail("missing resource: database");
+        }
+        Callable<Database> databaseCallable = databases.stream()
+                .findFirst()
+                .get()
+                .getCallable();
+
+        // sql
+        Collection<Resource<String>> sqls = context.getResources("sql");
+        if (sqls.isEmpty()) {
+            return Result.fail("missing resource: sql");
+        }
+        Callable<String> sqlCallable = sqls.stream()
+                .findFirst()
+                .get()
+                .getCallable();
+
+        // sql param
+        Collection<Resource<List<Object>>> sqlParam = context.getResources("sqlParam");
+        Callable<List<Object>> paramCallable = sqlParam.isEmpty() ? null : sqlParam.stream()
+                .findFirst()
+                .get()
+                .getCallable();
+
+        // return type
+        Type type;
+        Type genericReturnType = method.getGenericReturnType();
+        if (genericReturnType instanceof ParameterizedType) {
+            ParameterizedType parameterizedType = (ParameterizedType) genericReturnType;
+            if (parameterizedType.getRawType() != Flowable.class) {
+                return Result.fail("return type not Flowable");
+            }
+            type = parameterizedType.getActualTypeArguments()[0];
+        } else {
+            return Result.fail("return type not Flowable<T>");
+        }
+        return Result.success(ioContext -> {
+            Database database = databaseCallable.call(ioContext);
+            String sql = sqlCallable.call(ioContext);
+            SelectBuilder select = database.select(sql);
+
+            if (paramCallable != null) {
+                List<Object> params = paramCallable.call(ioContext);
+                select = select.parameters(params);
+            }
+
+            // refer: org.davidmoten.rx.jdbc.Util.getObject(java.sql.ResultSet, java.lang.Class<T>, int)
+            return select.get(this::rsToJsonObject);
+        });
+    }
+
+    private JSONObject rsToJsonObject(ResultSet rs) {
+        try {
+            ResultSetMetaData metaData = rs.getMetaData();
+            int columnCount = metaData.getColumnCount();
+            return IntStream.range(1, columnCount + 1)
+                    .boxed()
+                    .flatMap(i -> {
+                        try {
+                            Object value = getObject(rs, i);
+                            if (value == null) {
+                                return Stream.empty();
+                            }
+                            return Stream.of(Pair.of(metaData.getColumnName(i), value));
+                        } catch (SQLException e) {
+                            return Stream.empty();
+                        }
+                    })
+                    .collect(Collectors.toMap(Pair::getKey, Pair::getValue, (o, o2) -> o2, JSONObject::new));
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Object getObject(ResultSet rs, int i) {
+        try {
+            if (rs.getObject(i) == null) {
+                return null;
+            } else {
+                int type = rs.getMetaData().getColumnType(i);
+                if (type == 91) {
+                    return rs.getDate(i, Calendar.getInstance());
+                } else if (type == 92) {
+                    return rs.getTime(i, Calendar.getInstance());
+                } else if (type == 93) {
+                    return rs.getTimestamp(i, Calendar.getInstance());
+                } else {
+                    return rs.getObject(i);
+                }
+            }
+        } catch (SQLException e) {
+            throw new SQLRuntimeException(e);
+        }
+    }
+
+
+    @Override
+    public String resourceName() {
+        return "selectResult";
+    }
+}
